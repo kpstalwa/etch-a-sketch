9dbe109f477e2e7e638dd0ff4212dc9f09ac71f3
Karam Talwandi <k.talwandi@hotmail.com>
2017-06-13 14:30:54 -0400

    Changed colour variety, non overlapping yet.

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,16 +1,18 @@
 var numRow=16;
 var numCol=16;
 var grdiv=$('div#grid');
-var curColor="#800080";
+var curColor="red";
+var rainbow=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 $(document).ready(function(){
 	makeTable();
-	applyHover();
+	//applyHover();
 });
 function makeTable(){ //will make default 16x16 table
 	if (numRow === 0 && numCol===0){
 		alert("Please enter values greater than 0");
 	}
 	else {
+		curColor="red"; //set default upon grid change
 		ntable=document.createElement('table');
 		for (var i = 0; i < numRow; i++) {	
 			nrow=document.createElement('tr');
@@ -30,16 +32,21 @@ function getDimension(){
 	numCol=dims;
 	makeTable();
 }
-function getRandomColor(){
-	var color = Math.floor(Math.random() * 16777216).toString(16);
-	return '#000000'.slice(0, -color.length) + color;
-}
 function changeColor(){
-	applyHover();
-	curColor=getRandomColor();
-	applyHover();
+	var check=false;
+	var selectc=prompt("Colors: " + rainbow, "red");
+	for (var i=0; i<rainbow.length; i++){
+		if (rainbow[i] === selectc){
+			curColor=selectc;
+			check=true;
+			applyHover();
+		}
+	}
+	if(!check && selectc!=red){
+		alert("Please enter a valid rainbow color");
+	}
 }
-var cnt=0;
+//var cnt=0;
 function applyHover(){
 	var element = document.createElement('style'),
 	sheet;
@@ -49,17 +56,16 @@ document.head.appendChild(element);
 sheet = element.sheet;
 $("head").append('<style id="new-animations" type="text/css"></style>');
 var sheet = $("#new-animations").get(0).sheet;
-var styles = '.hover' + cnt + '{';
-console.log(curColor);
+var styles = '.hover' + curColor + '{';
 styles += 'background-color:' + curColor + ';';
 styles += '}';
 // Add the first CSS rule to the stylesheet
 sheet.insertRule(styles, 0);
 //cnt+=1;
 $("div#grid td").on('mouseover', function () {
-	$(this).addClass("hover" + cnt);
+	$(this).addClass("hover" + curColor);
+//cnt+=1;
 });
-cnt+=1;
 }
 
 
